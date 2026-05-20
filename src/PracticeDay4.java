8bf17d09d13756e59f67fc1614bfdbab581139b9
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-05-20 18:26:54 +0530

    PracticeDay4

---
+++ src/PracticeDay4.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+
+
+
+public class PracticeDay4 {
+    public static void main(String[] args){
+//Square Pattern
+        int n = 4;
+        for(int i = 0;i<n; i++){
+            for(int j =0;j<n; j++) System.out.print("*");
+            System.out.println();
+
+        }
+
+//        triangle pattern
+        int n2  = 5;
+        for(int i = 1;i<=n;i++){
+
+        }
+    }
+}
