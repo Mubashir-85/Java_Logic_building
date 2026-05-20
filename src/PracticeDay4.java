0bfefa17a3f69522081fa0976e6fec25d767a3be
Shaikh Mubashir <shaikhmubashirahmed123@gmail.com>
2026-05-20 18:42:56 +0530

    PracticeDay4

--- src/PracticeDay4.java
+++ src/PracticeDay4.java
@@ -16,7 +16,7 @@ public class PracticeDay4 {
         System.out.println();
 //        triangle pattern
         int n2  = 5;
-        for(int i = 1;i<=n;i++){
+        for(int i = 1;i<=n2;i++){
             for(int j =1; j<=i; j++) System.out.print("*");
             System.out.println();
 
@@ -24,10 +24,19 @@ public class PracticeDay4 {
         System.out.println();
 //            inverted triangle
             int n3 = 5;
-            for(int i = n;i>=1;i--){
+            for(int i = n3;i>=1;i--){
                 for(int j = 1; j<=i; j++) System.out.print("*");
                 System.out.println();
             }
+
+        System.out.println();
+//            Number Pattern
+            int n4=5;
+            for(int i =1; i<=n4; i++){
+                for(int j = 1;j<=i;j++) System.out.print(j);
+                System.out.println();
+            }
+
 
 
     }
